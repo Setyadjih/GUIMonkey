7d24f0e9d5e98c24b941a57d103ffb4ed45660c0
Hugo Setyadji <setyadjih@gmail.com>
2021-08-30 15:07:52 +0800

    test: add step adding test

--- tests/test_core.py
+++ tests/test_core.py
@@ -2,17 +2,31 @@ from rich import print
 from rich.progress import track
 
 from GUIMonkey.GUIMonkeyCore import TimelineManager
-
+from GUIMonkey import Steps
 
 def test_core_init():
     """Test initialization"""
-    test_timeline_name = "Test Timeline"
+    name = "Test Timeline"
 
     tm = TimelineManager()
-    timeline = tm.create_timeline(test_timeline_name)
+    timeline = tm.create_timeline(name)
 
-    assert timeline.name == test_timeline_name
+    assert timeline.name == name
 
 
+def test_step_add():
+    """Test adding steps to a timeline"""
+    name = "Test Steps"
 
+    manager = TimelineManager()
+    timeline = manager.create_timeline(name)
+
+    steps = [Steps.Delay(), Steps.KeyPress("win"), Steps.Write("Test")]
+
+    for step in track(steps, description="Adding steps..."):
+        timeline.add_step(step)
+
+    assert len(timeline.steps) == len(steps)
+
+    timeline.run_timeline()
 
